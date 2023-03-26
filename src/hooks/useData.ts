ad85641136b83635821f6a1cf392c7ac898109ab
raedgeorge <raedgeorge2014@gmail.com>
2023-03-26 20:32:32 +0300

    Create a generic data fetching Hook

---
+++ src/hooks/useData.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { CanceledError } from "axios";
+import apiClient from "../services/api-client";
+
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((response) => {
+        setData(response.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, isLoading };
+};
+
+export default useData;
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+}
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,33 +1,6 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "axios";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<GamesResponse>("/games", { signal: controller.signal })
-      .then((response) => {
-        setGames(response.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
 
@@ -43,9 +16,4 @@ export interface Game {
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
-}
-
-interface GamesResponse {
-  count: number;
-  results: Game[];
 }
