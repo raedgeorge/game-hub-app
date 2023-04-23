1835eb3be58c7022179a9fe43c277b55e0ef112e
raedgeorge <raedgeorge2014@gmail.com>
2023-04-23 21:43:32 +0300

    react part 2 - simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { GameQuery } from "../App";
 import APIClient, {FetchResponse} from "../services/api-client";
 import {useInfiniteQuery} from "@tanstack/react-query";
 const apiClient = new APIClient<Game>('/games');
+import ms from 'ms';
 
 const useGames = (gameQuery: GameQuery) =>
     useInfiniteQuery<FetchResponse<Game>, Error>({
@@ -16,7 +17,7 @@ const useGames = (gameQuery: GameQuery) =>
       getNextPageParam: (lastPage, allPages) => {
             return lastPage.next ? allPages.length + 1 : undefined
         },
-      staleTime: 24 * 60 * 60 * 1000, // 24 hours
+      staleTime: ms('24h'),
     })
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient, {FetchResponse} from "../services/api-client";
+import APIClient from "../services/api-client";
 import genres from "../data/genres";
+import ms from 'ms';
 
 const apiClient = new APIClient<Genre>('/genres');
 
 const useGenres = () =>
-  useQuery<FetchResponse<Genre>, Error>({
+  useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms('24h'),
     initialData: genres,
   });
 
